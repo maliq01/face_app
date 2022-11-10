3a2b7193c37124ff2eac02e8cc5c720f8fa7d4e5
maliq01 <61561654+maliq01@users.noreply.github.com>
2022-11-10 17:53:28 +0300

    face app

--- face_detect.py
+++ face_detect.py
@@ -1,3 +1,18 @@
 import cv2
+#importing the trained library and attaching it to a variable.
+trained_face_data = cv2.CascadeClassifier('haarcascade_fronatalface_defult.xml')
+
+#importing the image which we will detect the face.
+img = cv2.imread("pic.png")
+#turning the image to grayscale for the computer to read it well.
+grayscaled_img =cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+
+
+
+
+cv2.imshow("developed face detector", grayscaled_img)
+cv2.waitKey()
 
 print("code successful")
